4be08d05c934008d69f890e5cbde03ea567b39af
Christopher McDonald <chrismcband@gmail.com>
2014-06-21 14:50:45 +0100

    Added core express structure. Implemented providers.js with Provider base class. App requests ghs/btc price from CEX.io at interval specfied by config.

---
+++ providers.js
@@ -0,0 +1,69 @@
+var unirest = require('unirest');
+
+function Provider(options){
+  this.name = options.name;
+  this.restUrl = options.restUrl;
+  this.refreshInterval = options.refreshInterval || 30000;
+
+  this.last = 0;
+  this.bid = 0;
+  this.ask = 0;
+  this.high = 0;
+  this.low = 0;
+}
+
+Provider.prototype.refreshPrice = function(){
+  var that = this;
+
+  var request = unirest.get(this.restUrl);
+  request.set('Accepts', 'application/json');
+  request.end(function(response){
+    var data = JSON.parse(response.body);
+    var keys = ['last', 'bid', 'ask', 'high', 'low'];
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      that[key] = data[key];
+    }
+    console.log("Refreshed price for " + that.name + ", new price " + that.bid);
+  });
+};
+
+Provider.prototype.startListening = function(){
+  var that = this;
+
+  console.log("Started listening for " + this.name + " provider, querying " +
+    this.restUrl + " every " + this.refreshInterval+" ms...");
+
+  this.timerId = setInterval(
+    function() {
+      that.refreshPrice();
+    },
+    this.refreshInterval
+  );
+  this.refreshPrice();
+};
+
+Provider.prototype.stopListening = function(){
+  console.log("Stopped listening for " + this.name + " provider.");
+  clearInterval(this.timerId);
+};
+
+Provider.prototype.getBid = function(){
+  return this.bid;
+};
+
+Provider.prototype.getAsk = function(){
+  return this.ask;
+};
+
+Provider.prototype.getHigh = function(){
+  return this.high;
+};
+
+Provider.prototype.getLow = function(){
+  return this.low;
+};
+
+exports.createProvider = function(options){
+  return new Provider(options);
+};
